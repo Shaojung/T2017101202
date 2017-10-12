1e72e3d1d818064b65c08a2efb5aa91315f11db8
Shaojung Lu <shaojung@gmail.com>
2017-10-12 14:44:35 +0800

    ImageRequest and Picasso

--- app/src/main/java/com/example/teacher/t2017101202/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017101202/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.teacher.t2017101202;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import static com.example.teacher.t2017101202.R.id.imageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
         });
         queue.add(request);
         queue.start();
+
+        ImageRequest request1 = new ImageRequest("http://images.all-free-download.com/images/graphiclarge/butterfly_flower_01_hd_pictures_166973.jpg",
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        ImageView img = (ImageView) findViewById(imageView);
+                        img.setImageBitmap(response);
+
+                    }
+                }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request1);
+        queue.start();
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        Picasso.with(this)
+                .load("http://images.all-free-download.com/images/graphiclarge/pink_flowers_with_bees_hd_picture_166824.jpg")
+                .into(imageView);
 
     }
 }
